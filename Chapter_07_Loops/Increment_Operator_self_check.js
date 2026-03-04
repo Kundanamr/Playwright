0ae65a3213e7f067cdd851614b32492dd6ae46fb
Kundanamr <kundanamr@gmail.com>
2026-03-05 02:38:04 +0530

    Add increment operator self check loop practice

---
+++ Chapter_07_Loops/Increment_Operator_self_check.js
@@ -0,0 +1,9 @@
+let a = 10;
+a = a + 1;  //a++ also we can write
+let b = a++
+console.log(b); ///// its print 11
+
+//------------Anotrher case--------------------------
+let a1 = 11;
+let b1 = a++;
+console.log(b); ///// but ots print same 11
